71c13127b7fddcf1f98f906c624fb77003f239f0
clairenicholson078 <80160750+clairenicholson078@users.noreply.github.com>
2021-10-24 15:16:21 +0100

    Added image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.css";
+import gnome from './gnome.png'
+
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header"> </header>
       <div className="container shadow-lg">
-        <img src="images/garden-gnome-g1b4de28b3_1280.png" alt="garden-gnome"></img>
-        <Weather defaultCity="London" />
-        <div> <hr/>This project was coded by <a href="https://clairenicholsondigital.co.uk"> Claire Nicholson </a> and is open-sourced on <a href="https://github.com/clairenicholson078/weather-react"> Github </a></div>
+        <img src={gnome} alt="gnome" width="100px" /><br/>
+                <Weather defaultCity="London" />
+    <div> <hr/>This project was coded by <a href="https://clairenicholsondigital.co.uk"> Claire Nicholson </a> and is open-sourced on <a href="https://github.com/clairenicholson078/weather-react"> Github </a></div>
       </div>
     </div>
   );
